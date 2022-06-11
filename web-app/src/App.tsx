871995e9e88e48ede88f4342b498a7193b3ecd1d
Test <Test@test.com>
2022-06-11 09:38:20 +0300

    init custom 'tailwind css'

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavigationBar from './components/NavigationBar';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/colors.css';
+
 const App = () => {
   return (
-    <div className="App">
+    <div className="_ColorDepth-Bg-1">
       <NavigationBar />
       <p>React app</p>
     </div>
