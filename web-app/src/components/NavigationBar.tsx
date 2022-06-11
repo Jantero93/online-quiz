c4f0afb3b3b8a4b59a659d15f9b39404dee9636e
Test <Test@test.com>
2022-06-11 09:07:40 +0300

    init bootstrap react, eslint, prettier

---
+++ web-app/src/components/NavigationBar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavBar from 'react-bootstrap/NavBar';
+import NavDropdown from 'react-bootstrap/esm/NavDropdown';
+
+const NavigationBar = () => {
+  return (
+    <NavBar bg='light' expand='lg'>
+      <Container>
+        <NavBar.Brand href='#home'>React-Bootstrap</NavBar.Brand>
+        <NavBar.Toggle aria-controls='basic-navbar-nav' />
+        <NavBar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action/3.4'>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </NavBar.Collapse>
+      </Container>
+    </NavBar>
+  );
+};
+
+export default NavigationBar;
