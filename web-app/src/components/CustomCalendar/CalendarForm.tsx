cee8073e17f1c4a91b0619cd098ede9822f2490c
Test <Test@test.com>
2022-06-12 20:30:33 +0300

    Format number date col

--- web-app/src/components/CustomCalendar/CalendarForm.tsx
+++ web-app/src/components/CustomCalendar/CalendarForm.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 
 import { formatDate } from '../../utils/date';
 
+import Container from 'react-bootstrap/esm/Container';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+
 type Props = {
   clickedDate: Date;
 };
 
 const CalendarForm = ({ clickedDate }: Props) => {
   return (
-    <div className="_ColorDepth-Bg-3 d-flex justify-content-center">
-      <div className="mx-5">
-        <p>CalendarForm </p>
-        <p>{formatDate(clickedDate, 'DD.MM.YYYY')}</p>
-      </div>
-      <div className="mx-5">
-        <p>form</p>
-      </div>
-    </div>
+    <Container className="_ColorDepth-Bg-3 d-flex justify-content-center">
+      <Col xs={{ offset: 1, span: 2 }}>
+        <Row>Date</Row>
+        <Row>Daily</Row>
+        <Row>Week average</Row>
+      </Col>
+      <Col xs={{ span: 2 }}>
+        <Row>{formatDate(clickedDate, 'DD.MM.YYYY')}</Row>
+        <Row>82 kg</Row>
+        <Row>83,33 kg</Row>
+      </Col>
+      <Col xs={{ offset: 1, span: 6 }}>test 2</Col>
+    </Container>
   );
 };
 
