2d3491f51c213bafc86e052895d73ca61a152246
jani-pc\Jant <janzku1993@hotmail.com>
2022-06-18 18:44:58 +0300

    hash password to db

--- backend/src/main/java/com/server/backend/BackendApplication.java
+++ backend/src/main/java/com/server/backend/BackendApplication.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BackendApplication {
   public static void main(String[] args) {
+
     SpringApplication.run(BackendApplication.class, args);
   }
 
