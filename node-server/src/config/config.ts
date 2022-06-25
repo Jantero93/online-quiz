8191d275be3b3e7ad2a51bda916da2f40081a304
Jani <janzku1993@hotmail.com>
2022-06-25 13:49:01 +0300

    config dot env

--- node-server/src/config/config.ts
+++ node-server/src/config/config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 export default {
   PORT: process.env.PORT
--- node-server/src/config/db/DataSource.ts
+++ node-server/src/config/db/DataSource.ts
@@ -1,14 +1,15 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+
 import Entities from './indexEntities';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'admin',
-  database: 'online-quiz',
+  host: process.env.HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
   entities: Entities,
   synchronize: true,
   logging: false
