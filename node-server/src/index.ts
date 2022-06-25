56e568e0ee3df94d9d19f7c0ce1ea2e72bfc981b
Jani <janzku1993@hotmail.com>
2022-06-25 07:34:07 +0300

    env

--- node-server/src/index.ts
+++ node-server/src/index.ts
@@ -2,12 +2,14 @@ import { createServer } from 'http';
 
 import logger from './utils/logger';
 
+import config from './config/config';
+
 import app from './app';
 
 const server = createServer(app);
 
 try {
-  server.listen(8080, () => logger.info(`Connected successfully on port ${8080}`));
+  server.listen(config.PORT, () => logger.info(`Connected successfully on port ${config.PORT}`));
 } catch (error) {
   logger.error(error);
 }
