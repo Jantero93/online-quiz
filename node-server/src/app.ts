503c08e64bcb5dc79b93fd00fd49fba31bee8c6e
Jani <janzku1993@hotmail.com>
2022-06-25 07:50:12 +0300

    request logger

--- node-server/src/app.ts
+++ node-server/src/app.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import logger from './utils/logger';
 
+import requestLogger from './middleware/requestLogger';
+
 const app = express();
+
+app.use(requestLogger);
 
 app.get('/', (req: Request, res: Response) => {
   logger.info('test');
---
+++ node-server/src/middleware/requestLogger.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+import logger from '../utils/logger';
+
+const requestLogger = (req: Request, _res: Response, next: NextFunction) => {
+  const isParams = Object.keys(req.params).length > 0;
+
+  logger.info(`Method: ${req.method}`);
+  logger.info(`Path: ${req.path}`);
+  req.body && logger.info(`Body: ${req.body}`);
+  isParams && logger.info(`Params: ${JSON.stringify(req.params)}`);
+
+  next();
+};
+
+export default requestLogger;
