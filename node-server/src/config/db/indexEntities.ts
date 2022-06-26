e4c6b0cfe8f3a1927a4863854a09c2d8d9f72e88
Jani <janzku1993@hotmail.com>
2022-06-26 11:51:50 +0300

    delete node server

--- node-server/src/config/db/indexEntities.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../../model/entity/user';
-
-const entities = [User];
-
-export default entities;
--- node-server/src/repository/userRepository.ts
+++
@@ -1,9 +0,0 @@
-/** TypeORM */
-import AppDataSource from '../config/db/DataSource';
-
-/** Entity */
-import { User } from '../model/entity/user';
-
-const UserRepository = AppDataSource.getRepository(User).extend({});
-
-export default UserRepository;
