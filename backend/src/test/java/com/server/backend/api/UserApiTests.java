5ac117fdc8f2cf5e311b6fed36273850ecf7e0a8
Jani Sillanpaa <janzku1993@hotmail.com>
2022-07-04 20:31:49 +0300

    first integrate test

---
+++ backend/src/test/java/com/server/backend/api/UserApiTests.java
@@ -0,0 +1,29 @@
+package com.server.backend.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserApiTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUsers_withoutCredentials_returnsUnauthorized() throws Exception {
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isForbidden());
+    }
+}
